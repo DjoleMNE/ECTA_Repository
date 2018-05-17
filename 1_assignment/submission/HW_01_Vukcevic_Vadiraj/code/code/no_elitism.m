537c70598ad9ed539db8cc4513b3a5be0ba47f8a
Suppi14 <supriyavadi@gmail.com>
2018-05-17 11:57:04 +0200

    nothing to be done

---
+++ 1_assignment/submission/HW_01_Vukcevic_Vadiraj/code/code/no_elitism.m
@@ -0,0 +1,30 @@
+function eliteIds = no_elitism(fitness, p)
+%Elitism - Get indices of individual(s) to continue unchanged into next pop
+% - Standard Elitism:
+%   1) Find 1 best performing individual
+%   * Optional: In very large populations select a top percentage
+%
+% Syntax:  eliteId   = elitism(fitness, p)
+%
+% Inputs:
+%    fitness    - [M X 1] - Fitness of every individual in the population
+%    p          - _struct - Hyperparameter struct
+%     .elitePercent         - Percentage of pop to take as elites (min 1)
+%
+% Outputs:
+%    eliteIds   - [nElites X 1] - Indices of each elite
+%
+%
+% See also: selection, crossover, mutation, monkeyGa
+
+% Author: Adam Gaier
+% Bonn-Rhein-Sieg University of Applied Sciences (BRSU)
+% email: adam.gaier@h-brs.de
+% Feb 2018; Last revision: 20-Feb-2018
+
+%------------- BEGIN CODE --------------
+
+%% Here we just keep the first individual as an elite, can you do better?
+eliteIds = 1;
+
+%------------- END OF CODE --------------
---
+++ 1_assignment/submission/HW_01_Vukcevic_Vadiraj/code/code/solution/solved_elitism.m
@@ -0,0 +1,34 @@
+function eliteIds = elitism(fitness, p)
+%Elitism - Get indices of individual(s) to continue unchanged into next pop
+% - Standard Elitism:
+%   1) Find 1 best performing individual
+%   * Optional: In very large populations select a top percentage
+%
+% Syntax:  eliteId   = elitism(fitness, p)
+%
+% Inputs:
+%    fitness    - [M X 1] - Fitness of every individual in the population
+%    p          - _struct - Hyperparameter struct
+%     .elitePercent         - Percentage of pop to take as elites (min 1)
+%
+% Outputs:
+%    eliteIds   - [nElites X 1] - Indices of each elite
+%
+%
+% See also: selection, crossover, mutation, monkeyGa
+
+% Author: Adam Gaier
+% Bonn-Rhein-Sieg University of Applied Sciences (BRSU)
+% email: adam.gaier@h-brs.de
+% Feb 2018; Last revision: 20-Feb-2018
+
+%------------- BEGIN CODE --------------
+
+%% Here we just keep the first individual as an elite, can you do better?
+eliteIds = 1;
+
+%% ELITISM SOLUTION
+nElite = ceil(p.popSize * p.elitePerc);     % How many elites to take
+[~,iSorted]= sort(fitness,'descend');       % Sort population by fitness
+eliteIds = iSorted(1:nElite);               % Take top solutions
+%------------- END OF CODE --------------
